0bc9190e33a90d72f618c06d6d470200a8d617ab
GloryDream413 <glorydream413@gmail.com>
2023-08-31 16:39:14 +0900

    FrontEnd updated.

--- frontend/src/components/dream/Dream.jsx
+++ frontend/src/components/dream/Dream.jsx
@@ -3,69 +3,19 @@ import './dream.css';
 import { useDropzone } from 'react-dropzone';
 import axios from 'axios'
 import ClipLoader from "react-spinners/ClipLoader";
-import image_mask from '../../assets/mask_image.png'
 import { toast } from 'react-toastify';
 
-const baseUrl = "http://65.108.142.188:7777/";
+const baseUrl = "http://65.21.162.227:7777/";
 
 export const Dream = () => {
-  const [pictureRoute, setPictureRoute] = useState('')
-  const [maskRoute, setMaskRoute] = useState('')
-  const [nftRoute1, setNftRoute1] = useState('')
-  const [nftRoute2, setNftRoute2] = useState('')
-  const [nftRoute3, setNftRoute3] = useState('')
-  const [prompt, setPrompt] = useState('')
-  const onPromptChange = (event) => {
-    setPrompt(event.target.value);
-  };
-  
   useEffect(() => {
-    const image = new Image();
-    // Set the image source to the URL
-    image.src = image_mask;
-    // Wait for the image to load
-    image.onload = function() {
-      // Create a canvas element
-      const canvas = document.createElement('canvas');
-
-      // Set the canvas dimensions to match the image dimensions
-      canvas.width = image.width;
-      canvas.height = image.height;
-
-      // Draw the image on the canvas
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(image, 0, 0);
-
-      // Get the data URL for the canvas
-      const dataUrl = canvas.toDataURL('image/png');
-      setMaskRoute(dataUrl);
-      // dataUrl is the URI-based data URL for the image in PNG format
-    };
+    
   }, []);
 
   const onDrop = useCallback((acceptedFiles) => {
-    // Handle the uploaded files here
     console.log(acceptedFiles[0]);
     const file = acceptedFiles[0];
-    const imageUrl = URL.createObjectURL(file);
-    const image = new Image();
-    // Set the image source to the URL
-    image.src = imageUrl;
-
-    // Wait for the image to load
-    image.onload = function() {
-      // Create a canvas element
-      const canvas = document.createElement('canvas');
-      // Set the canvas dimensions to match the image dimensions
-      canvas.width = image.width;
-      canvas.height = image.height;
-      // Draw the image on the canvas
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(image, 0, 0);
-      // Get the data URL for the canvas
-      const dataUrl = canvas.toDataURL('image/png');
-      setPictureRoute(dataUrl);
-    };
+    const videoUrl = URL.createObjectURL(file);
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
@@ -167,31 +117,23 @@ export const Dream = () => {
   return (
     <div className="gpt3__header section__padding" id="home">
       <div className='generate'>
-        <h1><b>Generating dream rooms <span>using AI</span> for everyone.</b></h1>
+        <h1><b>Generating dream Video <span>using AI</span> for everyone.</b></h1>
       </div>
       <div className='generate'>
-        <button type="button" onClick={onGenerate}>Redesign your image</button>
+        <button type="button" onClick={onGenerate}>Generate your dream Video</button>
       </div>
       <div className="gpt3__header-content">
         <div className="original">
-          <h2>Upload Image</h2>
+          <h2>Upload Video (*.mp4)</h2>
           <div className="upload-container">
             <div {...getRootProps()} className={`box-container ${isDragActive ? 'active' : ''}`}>
               <input {...getInputProps()} />
               <p>{isDragActive ? 'Drop the file here' : 'Drag and drop file here'}</p>
             </div>
           </div>
-          <h2>Input Prompt</h2>
-          <textarea
-            className="desc"
-            placeholder="Enter your prompt"
-            name="prompt"
-            value={prompt}
-            onChange={onPromptChange}
-          ></textarea>
         </div>
         <div className="generated">
-          <h2>Uploaded Image</h2>
+          <h2>Uploaded Video</h2>
           {(pictureRoute === '') &&
             <div className='Uploaded'></div>
           }
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,5 +1,4 @@
-export {default as Dream} from './dream/Dream';
 export {default as Header} from './header/Header';
-export {default as Dashboard} from './dashboard/Dashboard';
 export {default as Footer} from './footer/Footer';
-export {default as GalleryItem} from './footer/Footer';
+export {default as Dashboard} from './dashboard/Dashboard';
+export {default as Dream} from './dream/Dream';
